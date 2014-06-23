acae1536ba80572400be769970e2fb3a54059c12
ajslaghu <ajslaghu@ajslaghu-THINK>
2014-06-23 17:00:51 +0200

    Update for index.php les serverload

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 require_once 'ocd.php';
 
 $ocd = new Ocd();
@@ -13,13 +8,10 @@ $ocd = new Ocd();
 $result = $ocd->search('rijksmuseum')
         ->add_facets(array('collection' => array()))
         ->add_filters(array('media_content_type' => array('terms' => array('image/jpeg', 'image/gif', 'image/png'))))
-        ->limit(150)
+        ->limit(21)
         ->query();
 $i = 0;
 foreach ($result as $item) {
     print("     ----->>>>>>  I $i " . $item['_id']. ' ' . $item['_source']['title'] . "\n"); 
     $i++;
-    //  echo "memory " . memory_get_usage() . "\n";
-//    if ($i > 4)
-//        break;
 }
