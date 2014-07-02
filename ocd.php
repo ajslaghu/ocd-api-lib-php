2f705d8332b6d0cb417d6a637acef89b353a5aa3
ajslaghu <ajslaghu@ajslaghu-THINK>
2014-07-02 11:32:36 +0200

    get_object working

--- ocd.php
+++ ocd.php
@@ -2,61 +2,72 @@
 
 class Ocd implements Iterator {
 
-    private $size = 5; // #results per API call; impact on memory
+    private $size = 20; // #results per API call; impact on memory
     private $query; // hash for input
     private $page; // the result (page)hash we provide during iteration n=0,..,n
     private $current; // pointer to array n=0,1,...,n
-    private $limit;
+    private $limit;//limit for results
 
     public function __construct() {
-        $this->api_url = 'http://api.opencultuurdata.nl';
-        $this->api_version = '/v0';
+        $this->api_url = 'http://api.opencultuurdata.nl/';
+        $this->api_version = 'v0';
     }
 
-// not tested yet
+    // todo
+    public function resolve() {
+        
+    }
+
 // returns version of the object that is not iteratable
 // GET /(source_id)/(object_id)/source
-    public function object($id) {
-// but source && similar is required
-        unset($this->query['query']);
+    public function get_object($id, $getsrc = false) {
+        // but source = allowed 
+        unset($this->query['query']); // but source = allowed 
         unset($this->query['similar']);
-
         $this->query['object_id'] = $id;
 
+        assert(!isset($this->query['query']) && !isset($this->query['similar']));
+        if (!$this->get_source()) {
+            throw new Exception('Object($id, getsrc)->Query() requires ->source($src) param');
+        }
+        $op = $this->get_source() . $this->query['object_id'] . ( $getsrc ? '/source' : null );
 
-        return $this;
+        return $this->rest($op, "GET");
     }
 
-// GET /similar/object-id / not tested yet
+
+// GET /similar/object-id returns similar objects
     public function similar($id) {
-// source is allowed
         unset($this->query['query']);
         unset($this->query['object_id']);
-
         $this->query['similar'] = $id;
-
         return $this;
-
-
-//return $this;
     }
 
 // sets the search query string
     public function search($search_str) {
-// source is allowed
         unset($this->query['similar']);
-
+        unset($this->query['object_id']);
         $this->query['query'] = $search_str;
         return $this;
     }
 
+    // Loads Query So Iteration can take place 
+    // Flushes pointers Throws Exceptions. Returns wat's working
+    public function query() {
+        $this->current = null;
+        if (!$this->get_results()) {
+            throw new Exception("QUERY FAILS");
+        }
+        return $this;
+    }
+
 // sets the (sole) source of the Query (Rijksmuseum, Stedelijk). Null == Everything
-    public function source($source) {
+    public function source($source=null) {
         $this->query['source'] = $source;
         return $this;
     }
 
-// (re)sets sort based on array of sort hashes
 // { "date":   { "order": "desc" }},{ "_score": { "order": "desc" }}
 // meta.source, meta.processing_started, meta.processing_finished, date, date_granularity, authors, _score
     public function sort($sort) {
@@ -92,19 +103,8 @@ class Ocd implements Iterator {
     }
 
 // sets the maximum results
-    public function limit($results) {
+    public function limit($results=null) {
         $this->limit = $results;
-        return $this;
-    }
-
-// Loads Query So Iteration can take place 
-// Flushes pointer 
-    public function query() {
-        $this->current = null;
-        if (!$this->get_results() || !$this->validate($this->current)) {
-            return FALSE;
-        }
-        $this->current = 0;
         return $this;
     }
 
@@ -113,20 +113,15 @@ class Ocd implements Iterator {
      */
 
     public function rewind() {
-//       echo "rewind\n";
-        if (!isset($this->current)) {
-            throw new Exception('$this->current is null. Meaning something failed');
-            //return FALSE;
-        }
-
         if ($this->current > $this->size) {// we are not in the first page.
             $this->current = 0;
-            $this->get_results();
-        }
+            if (!$this->get_results())
+                return FALSE;
+        } //other cases (null, or on first page)
         $this->current = 0;
         if (!$this->validate($this->current)) {
-            throw new Exception('!$this->validate($this->current)\n');
-            // return FALSE;
+            //throw new Exception('!$this->validate($this->current)\n');
+            return FALSE;
         }
         return TRUE;
     }
@@ -138,7 +133,7 @@ class Ocd implements Iterator {
      */
 
     public function current() {
-//  echo "current\n";
+        //  echo "current\n";
         if ($this->validate($this->current)) {
             return $this->page['hits']['hits'][($this->current % $this->size)];
         }
@@ -152,7 +147,7 @@ class Ocd implements Iterator {
      */
 
     public function key() {
-        echo "key\n";
+        //echo "key\n";
         if ($this->validate($this->current)) {
             return $this->current;
         }
@@ -160,10 +155,11 @@ class Ocd implements Iterator {
     }
 
     /* Returns the array value in the next place that's pointed to 
-     * by the internal array pointer, or FALSE if there are no more elements. */
+     * by the internal array pointer, or FALSE if there are no more elements. 
+     */
 
     public function next() {
-//   echo "next\n";
+        //   echo "next\n";
         $this->current++;
         if (!$this->validate($this->current)) {
             return FALSE;
@@ -181,79 +177,13 @@ class Ocd implements Iterator {
      */
 
     public function valid() {
-//    echo "valid\n";
+        //    echo "valid\n";
         return $this->validate($this->current);
-    }
-
-// private validation function
-    private function validate($pos) {
-        if (!isset($this->page['hits']['total'])) {
-            return FALSE;
-        }
-        if ($pos > ($this->page['hits']['total'] - 1)) {
-            return FALSE;
-        }
-        if (isset($this->limit) &&
-                $pos >= $this->limit) {
-            return FALSE;
-        }
-        return TRUE;
-    }
-
-    private function get_results() {
-        if (isset($this->query['object_id'])) {// handle get object_id (also handles src from object ID
-            assert(!isset($this->query['query']) && !isset($this->query['similar']));
-            $op = @$this->query['source'] . "/" . $this->query['object_id'];
-        } else {
-            assert($this->page_num() >= 0 && !isset($this->query['object_id']));
-            if (isset($this->query['query'])) {//query
-                assert(!isset($this->query['object_id']) && assert(!isset($this->query['similar'])));
-                $op = @$this->query['source'] . "/search";
-            } else if (isset($this->query['similar'])) {//handle similar
-                assert(!isset($this->query['query']));
-                $op = @$this->query['source'] . "/similar/" . $this->query['similar'];
-            }
-            $data['from'] = $this->page_num() * $this->size;
-            $data['size'] = isset($this->limit) ? (
-                    $this->limit < $data['from'] + $this->size ?
-                            $this->limit % $this->size : $this->size ) : $this->size;
-            foreach ($this->query as $key => $value) {
-                $data[$key] = $value;
-            }
-        }
-
-        $json = $this->rest($op, json_encode($data));
-        if (@$json['status'] == "error") {
-            throw new Exception($json['error']);
-            //return FALSE;
-        }
-        $this->page = $json;
-
-        return TRUE;
-    }
-
-    private function page_num() {
-        return (int) $this->current == 0 ? $this->current : $this->current / $this->size;
-    }
-
-// performs the actual curl request    
-    private function rest($op, $post_fields) {
-        echo $op . "\n";
-        var_dump($post_fields);
-        $ch = curl_init($this->api_uri() . $op);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($post_fields)));
-        $result = curl_exec($ch);
-        return json_decode($result, TRUE);
     }
 
 // returns the uri consisting of the url and api version
     public function api_uri() {
-        return $this->api_url . $this->api_version;
+        return $this->api_url . $this->api_version . "/";
     }
 
 // Returns total number of hits. We assume they are identical on every page (might be bold, ahem)
@@ -298,6 +228,77 @@ class Ocd implements Iterator {
     public function api_version($version) {
         $this->query['api_version'] = $version;
         return $this;
+    }
+
+    // private validation function
+    private function validate($pos) {
+        if (!isset($this->page['hits']['total'])) {
+            return FALSE;
+        }
+        if ($pos > ($this->page['hits']['total'] - 1)) {
+            return FALSE;
+        }
+        if (isset($this->limit) &&
+                $pos >= $this->limit) {
+            return FALSE;
+        }
+        return TRUE;
+    }
+
+    private function get_results() {
+        assert($this->page_num() >= 0 && !isset($this->query['object_id']));
+        if (isset($this->query['query'])) {//query
+            assert(!isset($this->query['object_id']) && assert(!isset($this->query['similar'])));
+            $op = $this->get_source() . "search";
+        } else if (isset($this->query['similar'])) {//handle similar
+            assert(!isset($this->query['query']));
+            $op = $this->get_source() . "similar/" . $this->query['similar'];
+        }
+        $data['from'] = $this->page_num() * $this->size;
+        $data['size'] = isset($this->limit) ? (
+                $this->limit < $data['from'] + $this->size ?
+                        $this->limit % $this->size : $this->size ) : $this->size;
+        foreach ($this->query as $key => $value) {
+            $data[$key] = $value;
+        }
+        $result = $this->rest($op, "POST", json_encode($data));
+       // var_dump($result);
+        $json = json_decode($result, TRUE);
+        if (@$json['status'] == "error") {
+            throw new Exception($json['error']);
+        }
+        $this->page = $json;
+        return TRUE;
+    }
+
+    private function get_source() {
+        if (!isset($this->query['source'])) {
+            return null;
+        }
+        return $this->query['source'] . "/";
+    }
+
+    private function page_num() {
+        return (int) $this->current == 0 ? $this->current : $this->current / $this->size;
+    }
+
+// performs the actual curl request    
+    private function rest($op, $request, $post_fields = null) {
+        print("rest() $request op " . $this->api_uri() . $op . " fields $post_fields\n");
+        $ch = curl_init($this->api_uri() . $op);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request);
+        if ($post_fields != null) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($post_fields)));
+        $result = curl_exec($ch);
+        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200) {
+            return $result;
+        }
+        throw new Exception("Http error " . curl_getinfo($ch, CURLINFO_HTTP_CODE));
     }
 
 }
